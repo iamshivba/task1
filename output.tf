5e9a22f2820f869314e2e432b9ab1ead345ec660
iamshivba <iamsnk2209@gmail.com>
2026-02-13 11:42:52 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "instance_public_ip" {
     description = "Public ip of all instance"
-    value = "aws_instance.ec2_instance.public_ip"
+    value = aws_instance.ec2_instance.public_ip
 }
 
 output "instance1_public_ip" {
     description = "Public ip of all instance1"
-    value = "aws_instance.ec2_instance1.public_ip"
+    value = aws_instance.ec2_instance1.public_ip
 }
 
 output "instance_id" {
      description = "instance id of all instance"
-     value = "aws_instance.ec2_instance.id"
+     value = aws_instance.ec2_instance.id
 }
 
 output "instance1_id" {
      description = "instance id of all instance1"
-     value = "aws_instance.ec2_instance1.id"
+     value = aws_instance.ec2_instance1.id
 }
 
 output "security_group_id" {
      description = "security group id"
-     value = "aws_security_group.task1.id"
+     value = aws_security_group.task1.id
 }
